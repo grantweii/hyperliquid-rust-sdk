6a1ca4e879c9b45f357f01de986a03d46a3d963d
Grant Wei <grantwei96@gmail.com>
2023-09-08 12:28:09 +1000

    again

--- src/signature/mod.rs
+++ src/signature/mod.rs
@@ -1,7 +1,6 @@
 pub(crate) mod agent;
 pub(crate) mod usdc_transfer;
 
-pub(crate) use create_signature::{
-    keccak, sign_l1_action, sign_usd_transfer_action, sign_with_agent,
-};
+pub use create_signature::{ keccak, sign_l1_action };
+pub(crate) use create_signature::{ sign_usd_transfer_action, sign_with_agent };
 pub mod create_signature;
